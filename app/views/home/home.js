4a7d027329dcfda99dff24a9d6bd20066442198e
Herb Torres <herb.torres@byhook.com>
2015-12-11 17:30:20 -0500

    audio and animation

--- app/views/home/home.js
+++ app/views/home/home.js
@@ -14,6 +14,8 @@ var Home = Backbone.View.extend({
   initialize: function() {
     this.listenTo(this.model, 'change:route', this.onRouteChange);
 
+    document.getElementById('theme').play();
+
     $('.slots-container').removeClass("hasWon");
 
     this.animate = new AnimationController();
@@ -21,7 +23,18 @@ var Home = Backbone.View.extend({
     this.gameMechanics();
   },
 
+  // Game logic and mechanics/animation
   gameMechanics: function() {
+
+    var content = {
+      data: this.model.get('data'),
+      winningSrc: _.sample(this.model.get('data').icons, 1),
+      losingSrc1: _.sample(this.model.get('data').icons, 1),
+      losingSrc2: _.sample(this.model.get('data').icons, 1),
+      losingSrc3: _.sample(this.model.get('data').icons, 1)
+    };
+
+    console.log(content);
 
     TweenMax.set('#lever', {y: -300, x: -40});
 
@@ -36,6 +49,7 @@ var Home = Backbone.View.extend({
       },
       onDragEnd:function() {
         console.log('lever pulled');
+        document.getElementById('crank').play();
 
         // game logic
         this.slotOne = [1,2,3];
@@ -47,11 +61,19 @@ var Home = Backbone.View.extend({
         if (this.choiceOne === this.choiceTwo) {
           console.log('won');
           this.hasWon = true;
+
           $('.slots-container').addClass("hasWon");
+
+          console.log(content.winningSrc);
+          $('.reveal').attr('src', content.winningSrc);
+
         } else {
           console.log('lost');
           this.hasWon = false;
           $('.slots-container').removeClass("hasWon");
+          $('.reveal-left').attr('src', content.losingSrc1);
+          $('.reveal-middle').attr('src', content.losingSrc2);
+          $('.reveal-right').attr('src', content.losingSrc3);
         }
 
         console.log(this.hasWon);
@@ -61,9 +83,14 @@ var Home = Backbone.View.extend({
           ease: Back.easeOut
         });
 
+        TweenMax.to('.reveal', 0.5, {
+          display: 'none',
+          autoAlpha: 0,
+          top: '60%'
+        });
+
         // slots spinning animation
         TweenMax.to('.inline-slots', 3, {
-          delay: 0.25,
           autoAlpha: 1,
           ease: Power4.easeInOut,
           onComplete: function() {
@@ -71,10 +98,26 @@ var Home = Backbone.View.extend({
               autoAlpha: 0,
               onComplete: function() { // when slots are done spinning....
                 if ($('.slots-container').hasClass('hasWon')) {
-                  window.location.href = '#greeting';
+                  TweenMax.staggerTo('.reveal', 0.5, {
+                    display: 'block',
+                    autoAlpha: 1,
+                    top: '63%',
+                    ease: Back.easeOut,
+                    onComplete: function() {
+                      document.getElementById('winner').play();
+                      window.location.href = '#greeting';
+                    }
+                  }, 0.25);
                   console.log('You have won, show somthing', this.hasWon);
                 } else {
                   console.log('You have lost, try again');
+                  document.getElementById('loser').play();
+                  TweenMax.staggerTo('.reveal', 0.5, {
+                    display: 'block',
+                    autoAlpha: 1,
+                    top: '63%',
+                    ease: Back.easeOut
+                  }, 0.25);
                 }
                 
               }
