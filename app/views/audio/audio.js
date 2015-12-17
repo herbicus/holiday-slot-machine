21e657f4444944656691bdba02167daf471330b7
Herb Torres <herb.torres@byhook.com>
2015-12-17 11:32:16 -0500

    audio toggle

--- app/views/audio/audio.js
+++ app/views/audio/audio.js
@@ -9,10 +9,37 @@ var AppAudio = Backbone.View.extend({
 
   template: _.template(template()),
 
+  events: {
+  	'click #volumeIcon': 'onClick'
+  },
+
   initialize: function() {
 
     this.$el.html(template());
+
+    if ( this.model.get('mobile') || this.model.get('tablet') ) {
+    	TweenMax.set($('#volumeIcon'), {display: 'none'});
+    }
     
+  },
+
+  onClick: function() {
+
+  	this.themeSong = document.querySelector('#theme');
+
+  	if (this.themeSong.paused === false) {
+  		this.themeSong.pause();
+  		TweenMax.to($('#volumeIcon'), 0.5, {
+  			autoAlpha: 0.35,
+  			ease: Power2.easeOut
+  		});
+  	} else {
+  		this.themeSong.play();
+  		TweenMax.to($('#volumeIcon'), 0.5, {
+  			autoAlpha: 1,
+  			ease: Power2.easeOut
+  		});
+  	}
   }
 
 });
